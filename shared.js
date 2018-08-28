9069b1ed71e43ac346d71bd647932560bec3d56c
Coding With Paul <codingwithpaul@Codings-MacBook-Pro.local>
2018-08-28 15:29:44 -0400

    Started to work on the mobile menu, added toggle button that will show or hide the mobile menu.

--- shared.js
+++ shared.js
@@ -1,8 +1,11 @@
 const backdrop = document.querySelector('.backdrop');
 const modal = document.querySelector('.modal');
 
+
 const selectPlanButton = document.querySelectorAll('.plan button');
 const closeModalButton = document.querySelector('.modal__action--negative');
+const closeBackdrop = document.querySelector('.backdrop');
+const toggleButton = document.querySelector('.toggle-button');
 
 selectPlanButton.forEach(button => {
     button.addEventListener( 'click', function() {
@@ -19,3 +22,13 @@ closeModalButton.addEventListener('click', function() {
     backdrop.style.display = 'none';
     modal.style.display = 'none'; 
 });
+
+closeBackdrop.addEventListener('click', function() {
+    console.log('backdrop was clicked');
+    backdrop.style.display = 'none';
+    modal.style.display = 'none';
+});
+
+toggleButton.addEventListener('click', function() {
+    console.log('toggle button clicked');
+})
