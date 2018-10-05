e4f2b8f3fd349d1db6c6db8435cec89694b5ca42
Paul Bratslavsky <paul.bratslavsky@gmail.com>
2018-10-05 15:22:39 -0400

    added transition to the mobile nav - now it comes in from right to left on tuggle button click

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,11 +6,11 @@ toggleButton.addEventListener('click', showHideMenu);
 
 function showHideMenu() {
     console.log('Toggle Button Clicked');
-    if ( mobileMenu.style.display === 'block') {
-        mobileMenu.style.display = 'none';
+    if ( mobileMenu.style.transform === 'translateX(0rem)') {
+        mobileMenu.style.transform = 'translateX(48rem)';
         // backdrop.style.display = 'none';
     } else {
-        mobileMenu.style.display = 'block';
+        mobileMenu.style.transform = 'translateX(0rem)';
         // backdrop.style.display = 'block';
     }
 };
